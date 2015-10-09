6adb7f38dbf2859827d6a469b70944d23c01bf2a
Ashley Hindle <hey@ashleyhindle.com>
2015-10-09 21:13:22 +0100

    New location of weights.db

--- src/WeightLog/Db.php
+++ src/WeightLog/Db.php
@@ -7,9 +7,9 @@ class Db
 {
     public static function getInstance()
     {
-        $basepath = $basepath = realpath(__DIR__ . '/../../');
+        $basepath = realpath(__DIR__ . '/../../');
 
-        $db = new ExtendedPdo('sqlite:' . $basepath . 'weights.db');
+        $db = new ExtendedPdo('sqlite:' . $basepath . '/weights.db');
         $db->exec("CREATE TABLE IF NOT EXISTS persons (id INTEGER PRIMARY KEY, timeadded INTEGER, currentweight REAL, telegramid INTEGER, firstname TEXT, username TEXT, token TEXT)");
         $db->exec("CREATE TABLE IF NOT EXISTS weights (id INTEGER PRIMARY KEY, timestamp INTEGER, weight REAL, personid INTEGER)");
         
